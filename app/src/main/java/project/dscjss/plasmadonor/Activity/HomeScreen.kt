ca4d734fc21b95728d7c616ac9924db15a999425
Vatsal kesarwani <vatsalkesarwani12@gmail.com>
2020-09-20 17:26:52 +0530

    login screen views wrapped dependencies added home screen created

---
+++ app/src/main/java/project/dscjss/plasmadonor/Activity/HomeScreen.kt
@@ -0,0 +1,12 @@
+package project.dscjss.plasmadonor.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.dscjss.plasmadonor.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
--- app/src/main/java/project/dscjss/plasmadonor/Activity/SplashActivity.kt
+++ app/src/main/java/project/dscjss/plasmadonor/Activity/SplashActivity.kt
@@ -3,16 +3,21 @@ package project.dscjss.plasmadonor.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import project.dscjss.plasmadonor.R
-import java.lang.Thread.sleep
 
 class SplashActivity : AppCompatActivity() {
+
+    val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sleep(2000);
+        handler.postDelayed(Runnable {
+            startActivity(Intent(applicationContext , LoginActivity::class.java))
+            finish()
+        }, 2000)
 
-        startActivity(Intent(applicationContext , LoginActivity::class.java))
     }
 }
