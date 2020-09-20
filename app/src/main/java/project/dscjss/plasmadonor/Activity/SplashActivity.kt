aa14a175337de324271a88b13a731054a3caf8ad
Vatsal kesarwani <vatsalkesarwani12@gmail.com>
2020-09-20 18:09:23 +0530

    AppConfig.kt added to store data for the shared prefs

--- app/src/main/java/project/dscjss/plasmadonor/Activity/SplashActivity.kt
+++ app/src/main/java/project/dscjss/plasmadonor/Activity/SplashActivity.kt
@@ -1,6 +1,7 @@
 package project.dscjss.plasmadonor.Activity
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
---
+++ app/src/main/java/project/dscjss/plasmadonor/Data/AppConfig.kt
@@ -0,0 +1,6 @@
+package project.dscjss.plasmadonor.Data
+
+class AppConfig {
+    var SHARED_PREF = "SHARED_PREF"
+    var LOGIN_STATE = "LOGIN_STATE"
+}
