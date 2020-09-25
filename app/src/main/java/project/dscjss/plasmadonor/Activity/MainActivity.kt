c3e76a38d822659271d818eaf02033ba98c76e92
Nakul Sharma <rnqnkl@gmail.com>
2020-09-25 19:27:42 +0530

    Navigation drawer added. About us and Logout menu items' functionality yet to be implemented.

--- app/src/main/java/project/dscjss/plasmadonor/Activity/MainActivity.kt
+++ app/src/main/java/project/dscjss/plasmadonor/Activity/MainActivity.kt
@@ -1,20 +1,33 @@
 package project.dscjss.plasmadonor.Activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
-import project.dscjss.plasmadonor.Fragment.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import project.dscjss.plasmadonor.Fragment.*
 import project.dscjss.plasmadonor.Interface.FragmentChangeInterface
 import project.dscjss.plasmadonor.R
 
-class MainActivity : AppCompatActivity(), FragmentChangeInterface {
+class MainActivity : AppCompatActivity(), FragmentChangeInterface, NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen)
+        setContentView(R.layout.activity_main)
+        val drawer=findViewById<DrawerLayout>(R.id.drawerLayout)
+        val toggle = ActionBarDrawerToggle(this,drawer,R.string.open,R.string.close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.mainFrame, ProfileFragment())
@@ -33,5 +46,36 @@ class MainActivity : AppCompatActivity(), FragmentChangeInterface {
                 .replace(R.id.mainFrame, fragment).commit()
         }
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> initiate(FeedsFragment())
+            R.id.addDonor -> initiate(DonorFormFragment())
+            R.id.addPatient -> initiate(PatientFormFragment())
+            R.id.profile -> initiate(ProfileFragment())
+            R.id.faq -> initiate(FaqFragment())
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun initiate(fragment:Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFrame, fragment).commit()
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+                .onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 }
